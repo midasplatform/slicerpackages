a8a7629921b896f299b5ec5cc92d696f55d6b1ec
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-10-07 19:05:18 -0400

    ENH: refs #0625. Style main view

--- public/js/index/slicerpackages.index.js
+++ public/js/index/slicerpackages.index.js
@@ -1,2 +1,5 @@
-$(document).ready(function() {   
+$(document).ready(function() {
+
+  var webroot = $('.webroot').val();
+  $('.packageLinks li a').prepend('<img alt="" src="' + webroot + '/core/public/images/icons/download.png" />');
 });
