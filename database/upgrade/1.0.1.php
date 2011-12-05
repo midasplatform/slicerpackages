abf849075b3d6c515dd65e7b7654463de544fd58
Zach Mullen <zach.mullen@kitware.com>
2011-12-05 11:30:20 -0500

    Add new metadata columns to package table (product name, codebase, build date)

---
+++ database/upgrade/1.0.1.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * We added a few metadata fields to the packages table
+ *  -Product name
+ *  -Codebase name
+ *  -Repository checkout date used to build the package
+ */
+class Slicerpackages_Upgrade_1_0_1 extends MIDASUpgrade
+{
+  public function preUpgrade()
+    {
+    }
+
+  /** Mysql upgrade */
+  public function mysql()
+    {
+    $this->db->query("ALTER TABLE `slicerpackages_package`
+                      ADD COLUMN `productname` varchar(256) NOT NULL DEFAULT ''");
+    $this->db->query("ALTER TABLE `slicerpackages_package`
+                      ADD COLUMN `codebase` varchar(256) NOT NULL DEFAULT ''");
+    $this->db->query("ALTER TABLE `slicerpackages_package`
+                      ADD COLUMN `checkoutdate` timestamp NULL DEFAULT NULL");
+    }
+
+  /** Pgsql upgrade */
+  public function pgsql()
+    {
+    $this->db->query("ALTER TABLE slicerpackages_package
+                      ADD COLUMN productname character varying(256) NOT NULL DEFAULT ''");
+    $this->db->query("ALTER TABLE slicerpackages_package
+                      ADD COLUMN codebase character varying(256) NOT NULL DEFAULT ''");
+    $this->db->query("ALTER TABLE slicerpackages_package
+                      ADD COLUMN checkoutdate timestamp without time zone NULL DEFAULT NULL");
+    }
+
+  public function postUpgrade()
+    {
+    }
+}
+?>
